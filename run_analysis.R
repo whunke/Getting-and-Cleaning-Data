93a4f68286bb37beeb7f842ffdd632a3f07f9749
William Hunke <whunke@gmail.com>
2015-05-13 20:25:04 -0400

    added code for all 5 project tasks

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,5 @@
+library(plyr)
+
 # The purpose of this script is to pull and clean wearable computing data to create a tidy data set.
 # The source for this data is https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip.
 
@@ -5,32 +7,51 @@
 
 tmp1 <- read.table("UCI HAR Dataset/train/X_train.txt")
 tmp2 <- read.table("UCI HAR Dataset/test/X_test.txt")
-X <- rbind(tmp1, tmp2)
+x <- rbind(tmp1, tmp2)
 
 tmp1 <- read.table("UCI HAR Dataset/train/subject_train.txt")
 tmp2 <- read.table("UCI HAR Dataset/test/subject_test.txt")
-S <- rbind(tmp1, tmp2)
+s <- rbind(tmp1, tmp2)
 
 tmp1 <- read.table("UCI HAR Dataset/train/y_train.txt")
 tmp2 <- read.table("UCI HAR Dataset/test/y_test.txt")
-Y <- rbind(tmp1, tmp2)
+y <- rbind(tmp1, tmp2)
 
 # The second portion of this code extracts only the measurements on the mean and standard deviation for each measurement.
 
 features <- read.table("UCI HAR Dataset/features.txt")
 mean_and_std_dev <- grep("-mean\\(\\)|-std\\(\\)", features[, 2])
-X <- X[, mean_and_std_dev]
-names(X) <- features[mean_and_std_dev, 2]
+x <- x[, mean_and_std_dev]
+names(x) <- features[mean_and_std_dev, 2]
 
 # The third portion of this code uses descriptive activity names to name the activities in the data set.
 
 activities <- read.table("UCI HAR Dataset/activity_labels.txt")
-Y[, 1] <- activities[Y[, 1], 2]
-names(Y) <- "activity"
+y[, 1] <- activities[y[, 1], 2]
+names(y) <- "activity"
 
 # The fourth portion of this code appropriately labels the dataset with descriptive variable names.
 
-names(S) <- "subject"
-cleaned <- cbind(S, Y, X)
+names(s) <- "subject"
+cleaned <- cbind(s, y, x)
 write.table(cleaned, "clean_data.txt")
+
+# The fifth portion of this code creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+uniqueSubjects = unique(s)[,1]
+numSubjects = length(unique(s)[,1])
+numActivities = length(activities[,1])
+numCols = dim(cleaned)[2]
+result = cleaned[1:(numSubjects*numActivities), ]
+row = 1
+for (s in 1:numSubjects) {
+    for (a in 1:numActivities) {
+        result[row, 1] = uniqueSubjects[s]
+        result[row, 2] = activities[a, 2]
+        tmp <- cleaned[cleaned$subject==s & cleaned$activity==activities[a, 2], ]
+        result[row, 3:numCols] <- colMeans(tmp[, 3:numCols])
+        row = row+1
+    }
+}
+write.table(result, "averages.txt")
 
